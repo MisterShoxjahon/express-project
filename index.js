c00183a9e01ddd39e2696b2ec69df039b8875b0b
MisterShoxjahon <sobirzanovsohzahon@gmail.com>
2026-04-13 16:24:03 +0500

    #4. Layout

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import {fileURLToPath} from 'url'
-import {engine} from 'express-handlebars'
+import {engine, create} from 'express-handlebars'
 
 const __fileName = fileURLToPath(import.meta.url)
 const __dirName = dirname(__fileName)
@@ -9,8 +9,13 @@ const __dirName = dirname(__fileName)
 
 const app = express()
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+const hbs = create({ // handlebars is too long-written, so we are creating a hbs to put like main.hbs
+	defaultLayout: 'main',
+	extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
 app.set('views', './views')
 
 
